503db1a4163afe2a9e48409bbc43e4f45d1e4ee0
SandrineYogaratnam <Sandrine.Yogaratnam@gmail.com>
2022-02-18 21:51:03 +0100

    commit du fichier KaggleBike_Sharing.py modifié

--- KaggleBikeSharing/Kaggle_BikeSharing.py
+++ KaggleBikeSharing/Kaggle_BikeSharing.py
@@ -173,6 +173,24 @@ plt.ylabel('nombre de locations de vélos')
 plt.title('Moyenne des locations de velos pour differentes conditions meteorologiques')
 plt.show()
 
+## Suggestions -Partie 1 ##
+
+#Information base de données
+df.dtypes
+
+#Combiner utilisateurs enregistrées occassionnels par Season
+df.groupby('season')['registered', 'casual'].mean().plot(kind = 'bar', color = ["green", "olive"], edgecolor='black')
+df.groupby('season')['registered', 'casual'].mean()
+
+#Combiner utilisateurs enregistrées occassionnels par Workingday
+df.groupby('workingday')['registered', 'casual'].mean().plot(kind='bar', color = ["green", "olive"], edgecolor='black')
+df.groupby('workingday')['registered', "casual"].mean()
+        
+#Combiner utilisateurs enregistrées occassionnels par Holiday
+df.groupby('holiday')['registered', 'casual'].mean().plot(kind='bar', color = ["green", "olive"], edgecolor='black')
+df.groupby('holiday')['registered', "casual"].mean()
+
+
 ################### 
 ################### Influence du vent, de la température et de l'humidité
 ################### 
@@ -200,7 +218,7 @@ plt.show()
 ################### Conclusion et choix des paramètres influents
 ################### 
 
-##On calcule matrice de corrélation pour 
+##On calcule la  matrice de corrélation pour 
 ##retirer les variables corrélées
 
 df.corr()
@@ -214,6 +232,74 @@ df1=df.drop(['workingday','datetime','season','atemp','holiday','registered','ca
 target=df1['count'].values #ensemble des outputs à prédire (yi)
 
 train=df1.drop('count',axis=1) #ensemble des données (xi)
+
+
+####Création variable Mois, Jours en lettre
+months_letter = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+day_letter = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+#Constitution dataframe mois en fonction du comptage moyen
+df_months = pd.DataFrame(df.groupby("month")["count"].mean()).reset_index()
+df_months_sort = monthAggregated.sort_values(by="count",ascending=False)
+
+#Plot 1 : Comptage moyen par mois
+import matplotlib.pyplot as plt
+
+sns.set(style='whitegrid')
+
+fig = plt.figure(figsize=(12, 8))
+plt=sns.barplot(x="month",y="count",order=months_letter, data=df_months_sort, color = "lightblue")
+plt.set_xticklabels(plt.get_xticklabels(), rotation=45, ha='right')
+
+plt.set_xlabel('Mois', fontsize = 15)
+plt.set_ylabel('Comptage', fontsize = 15)
+plt.set_title("Comptage moyen d'utilisateurs par mois", fontsize=20, fontweight='bold')
+
+
+############ espace
+
+#Dataframe saison, heure regrouper en fonction du comptage
+
+df_heure_saison = pd.DataFrame(df.groupby(["hour","season"],sort=True)["count"].mean()).reset_index()
+
+### Plot 2 : Nombre moyen d'utilisateurs par heure de la journée et par saison
+
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(12, 8))
+plt2 = sns.pointplot(x="hour", y="count",hue="season", data=df_heure_saison, join=True, palette = "pastel")
+
+
+plt2.set_xlabel("Créneau d'utilisation dans la journée (unité : heures)", fontsize = 13)
+plt2.set_ylabel("Nombre d'utilisateurs", fontsize = 13)
+plt2.set_title("Nombre moyen d'utilisateurs par heure de la journée durant les 4 saisons", fontsize=15, fontweight='bold')
+
+
+#Dataframe heure, jours regrouper en fonction du comptage
+df_heure_jours = pd.DataFrame(df.groupby(["hour","weekday"],sort=True)["count"].mean()).reset_index()
+
+############
+
+#Dataframe heure, jours regrouper en fonction du comptage
+
+df_heure_jours = pd.DataFrame(df.groupby(["hour","weekday"],sort=True)["count"].mean()).reset_index()
+
+#Plot 3 : Nombre moyen d'utilisateurs par heure de la journée pendant les différents jours de la semaine
+
+
+import matplotlib.pyplot as plt
+fig = plt.figure(figsize=(12, 8))
+        plt3 = sns.pointplot(x="hour", y="count",hue="weekday",hue_order=hueOrder, data=df_heure_jours, join=True, palette = "pastel")
+
+plt3.set_xlabel("Créneau d'utilisation dans la journée (unité : heures)", fontsize = 13)
+
+plt3.set_ylabel("Nombre d'utilisateurs", fontsize = 13)
+plt3.set_title("Nombre moyen d'utilisateurs par heure de la journée pendant les différents joursde la semaine", fontsize=15, fontweight='bold')
+
+## 2) Etude de la corrélation
+
+import seaborn as sns
+sns.heatmap(df.corr())
+
 
 ## Split aléatoire entre données d'entrainement
 ## et donnes test 
@@ -239,6 +325,7 @@ def plot_learning_curve(estimator, title, X, y, cv=None, n_jobs=1, train_sizes=n
     plt.plot(train_sizes, test_scores_mean, 'o-', color="g", label="Cross-validation score")
     plt.legend(loc="best")
     return plt
+
 
 ################### 
 ################### Regression Linéaire
@@ -334,4 +421,145 @@ print('La moyenne totale de la demande est : ', df['count'].mean())
 print('En semaine, entre 17 et 19h : ', peak_soir['count'].mean())
 print('En semaine, entre 7 et 9h : ', peak_matin['count'].mean())
 print('En week-end, entre 12 et 16h : ', peak_we['count'].mean())
+
+
+################################################################################
+###################################Suggestion Partie 2##########################
+
+
+####Suggestion Modèle####
+
+
+#####Combinaison de la base train et test#####
+
+#####Reprendre la base initiale######
+
+
+####On merge les 2 tables######
+
+data = df.append(de)
+data.reset_index(inplace=True)
+data.drop('index',inplace=True,axis=1)
+
+
+
+##Feature Engineering
+
+data["date"] = data.datetime.apply(lambda x : x.split()[0])
+data["hour"] = data.datetime.apply(lambda x : x.split()[1].split(":")[0]).astype("int")
+data["year"] = data.datetime.apply(lambda x : x.split()[0].split("-")[0])
+data["weekday"] = data.date.apply(lambda dateString : datetime.strptime(dateString,"%Y-%m-%d").weekday())
+data["month"] = data.date.apply(lambda dateString : datetime.strptime(dateString,"%Y-%m-%d").month)
+
+data
+
+
+
+## Nombre de valeurs manquantes
+
+data.isnull().sum()
+
+
+#### Catégoriser les variables
+
+categoricalFeatureNames=["season","holiday","workingday","weather","weekday","month","year","hour"]
+
+numericalFeatureNames = ["temp","humidity","windspeed","atemp"]
+
+##On supprimes les NA
+
+dropFeatures = ['casual',"count","datetime","date","registered"]
+
+for var in categoricalFeatureNames:
+    data[var] = data[var].astype("category")
+
+
+
+
+
+### On splitte notre base de donnée en train,test, split
+
+dataTrain = data[pd.notnull(data['count'])].sort_values(by=["datetime"])
+dataTest = data[~pd.notnull(data['count'])].sort_values(by=["datetime"])
+
+dataTrain
+
+datetimecol = dataTest["datetime"]
+yLabels = dataTrain["count"]
+
+yLablesRegistered = dataTrain["registered"]
+yLablesCasual = dataTrain["casual"]
+
+dataTrain  = dataTrain.drop(dropFeatures,axis=1)
+dataTest  = dataTest.drop(dropFeatures,axis=1)
+
+
+
+
+### RMSLE score
+
+def rmsle(y, y_,convertExp=True):
+    if convertExp:
+        y = np.exp(y),
+        y_ = np.exp(y_)
+    log1 = np.nan_to_num(np.array([np.log(v + 1) for v in y]))
+    log2 = np.nan_to_num(np.array([np.log(v + 1) for v in y_]))
+    calc = (log1 - log2) ** 2
+    return np.sqrt(np.mean(calc))
+
+
+
+
+### Regularisation Ridge
+
+### import des packages
+
+
+from sklearn.linear_model import LinearRegression,Ridge,Lasso
+from sklearn.model_selection import GridSearchCV
+from sklearn import metrics
+import warnings
+pd.options.mode.chained_assignment = None
+warnings.filterwarnings("ignore", category=DeprecationWarning
+
+ridge_m_ = Ridge()
+ridge_params_ = { 'max_iter':[3000],'alpha':[0.1, 1, 2, 3, 4, 10, 30,100,200,300,400,800,900,1000]}
+rmsle_scorer = metrics.make_scorer(rmsle, greater_is_better=False)
+grid_ridge_m = GridSearchCV( ridge_m_,
+                          ridge_params_,
+                          scoring = rmsle_scorer,
+                          cv=5)
+
+yLabelsLog = np.log1p(yLabels)
+grid_ridge_m.fit( dataTrain, yLabelsLog )
+preds = grid_ridge_m.predict(X= dataTrain)
+print (grid_ridge_m.best_params_)
+print ("RMSLE Value For Ridge Regression: ",rmsle(np.exp(yLabelsLog),np.exp(preds),False))
+
+
+
+### Regularisation Modèle  Lasso
+
+lasso_m_ = Lasso()
+
+alpha  = 1/np.array([0.1, 1, 2, 3, 4, 10, 30,100,200,300,400,800,900,1000])
+lasso_params_ = { 'max_iter':[3000],'alpha':alpha}
+
+grid_lasso_m = GridSearchCV( lasso_m_,lasso_params_,scoring = rmsle_scorer,cv=5)
+yLabelsLog = np.log1p(yLabels)
+grid_lasso_m.fit( dataTrain, yLabels, Log )
+preds = grid_lasso_m.predict(X= dataTrain)
+print (grid_lasso_m.best_params_)
+print ("RMSLE Value For Lasso Regression: ",rmsle(np.exp(yLabelsLog),np.exp(preds),False))
+
+
+
+
+
+
+
+
+
+
+
 
